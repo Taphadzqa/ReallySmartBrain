4666c9715fbb2a03c37f2115642043186b1535a7
Tafadzwa Mugari <taphadzqa@outlook.com>
2023-01-28 20:53:01 +0200

    Adding a new comment

--- brain.py
+++ brain.py
@@ -5,7 +5,7 @@ import os
 exec_path = os.getcwd()
  
 prediction = ImageClassification()
-# SqueezeNet model also no longer exists, now the fastest is MobileNetV2
+
 prediction.setModelTypeAsMobileNetV2()
 prediction.setModelPath(os.path.join(exec_path, 'mobilenet_v2-b0353104.pth'))
 prediction.loadModel()
@@ -13,3 +13,5 @@ prediction.loadModel()
 predctions, probabilities = prediction.classifyImage(os.path.join(exec_path,'godzilla.jpg'), result_count=5)
 for eachPred, eachProb in zip(predctions, probabilities):
     print(f'{eachPred} : {eachProb}') 
+
+# Some comment goes here.
